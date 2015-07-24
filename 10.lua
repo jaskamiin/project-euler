fff8f88ef811b2425e4349fc2ee8b078090824c9
jaska jaskaalainen <jaskamiin@gmail.com>
2015-07-24 06:19:38 -0400

    add problems 9 and 10

---
+++ 10.lua
@@ -0,0 +1,22 @@
+﻿require "bit32"
+require "math"
+
+function isPrime(x)
+    local n = math.ceil(math.sqrt(x))
+    if x == 2 or x == 3 then return true end
+    if bit.band(x,1)==0 or x == 0 or x == 1 then return false end
+    for i=3, n, 2 do
+       if x%1 == 0 then return false end
+    end
+    return true
+end
+
+function Main()
+    local sum = 2
+    for i=3, 2000000, 2 do 
+        if isPrime(i) then sum = sum + i end 
+    end
+    print(sum)    
+end
+
+Main()
---
+++ 9.lua
@@ -0,0 +1,16 @@
+﻿function Main()
+    for a=3, 333, 1 do
+        for b=4, 500, 1 do
+            for c=5, 500, 1 do
+                if a^2 + b^2 == c^2 then
+                    if a+b+c == 1000 then
+                        print(a*b*c)
+                        return true
+                    end
+                end
+            end
+        end
+    end
+end
+
+Main()

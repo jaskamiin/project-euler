9f84b92e94938104a153070d2ca26415c11ad561
jaska jaskaalainen <jaskamiin@gmail.com>
2015-07-25 06:30:36 -0400

    problem 5 lua - works, but quick code makes slow execution

---
+++ lua/5.lua
@@ -0,0 +1,19 @@
+﻿--works, but slowly
+
+function Main()
+    local flag,inc = false, 1
+    repeat
+        for i=1,21,1 do
+            if i == 21 then 
+                flag = true
+                goto continue
+            end
+            if inc%i ~= 0 then goto continue end
+        end
+        ::continue::
+        inc = inc + 1
+    until flag == true
+    print(inc-1)
+end
+
+Main()
